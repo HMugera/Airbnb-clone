3ea1e49229eab5b424d8e11749fae1a606cf70b2
Hmugera <mugerahughes@gmail.com>
2020-11-15 01:06:51 +0300

    working on date search component,setting up UI

--- src/Banner/Banner.js
+++ src/Banner/Banner.js
@@ -3,14 +3,16 @@ import React,{useState} from 'react'
 import "./Banner.css"
 
 import { Button } from '@material-ui/core'
+import Search from "../Search/Search"
 function Banner() {
 const [showSearch,setShowSearch]= useState(false)
 
     return (
        <div className="banner">
            <div className="banner__search">
-               {showSearch && <h1>SHOW DATE PICKER</h1>}
-               <Button variant="outlined" className="banner__searchButton" onClick={()=>setShowSearch(!showSearch)}>Search Dates</Button>
+               {showSearch && <Search/>}
+               <Button variant="outlined" className="banner__searchButton" onClick={()=>setShowSearch(!showSearch)}>
+                   {showSearch ?`Hide`:`Search Date`}</Button>
            </div>
           <div className="banner__info">
               <h1>Get out and strech your imagiantion</h1>
