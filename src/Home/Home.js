554185a8ad17229255f77f4bc0894ab270631411
Hmugera <mugerahughes@gmail.com>
2020-11-15 01:59:12 +0300

    setting up card component

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,11 +1,49 @@
 import React from 'react'
 import './Home.css'
-
+import Card from "../Card/Card"
 import Banner from '../Banner/Banner'
 function Home() {
     return (
         <div className="home">
            <Banner/>
+          
+          <div className="home__section">
+          <Card 
+             src="https://media.nomadicmatt.com/2018/apartment.jpg"
+              title="Entire homes"
+              description="Comfortable private places,with room for friends or family"
+              />
+          <Card 
+             src="https://media.nomadicmatt.com/2018/apartment.jpg"
+              title="Entire homes"
+              description="Comfortable private places,with room for friends or family"
+              />
+          <Card 
+             src="https://media.nomadicmatt.com/2018/apartment.jpg"
+              title="Entire homes"
+              description="Comfortable private places,with room for friends or family"
+              />
+            
+  
+          </div>
+          <div className="home__section">
+          <Card 
+             src="https://media.nomadicmatt.com/2018/apartment.jpg"
+              title="Entire homes"
+              description="Comfortable private places,with room for friends or family"
+              />
+                  <Card 
+             src="https://media.nomadicmatt.com/2018/apartment.jpg"
+              title="Entire homes"
+              description="Comfortable private places,with room for friends or family"
+              />
+                   <Card 
+             src="https://media.nomadicmatt.com/2018/apartment.jpg"
+              title="Entire homes"
+              description="Comfortable private places,with room for friends or family"
+              />
+              
+          </div>
         </div>
     )
 }
